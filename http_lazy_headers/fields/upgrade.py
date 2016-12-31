b6098d5b4578547fea192fe96998dbc43ef9dcb0
nitely <ecastroborsani@gmail.com>
2016-12-31 03:47:47 -0300

    upgrade values check

--- http_lazy_headers/fields/upgrade.py
+++ http_lazy_headers/fields/upgrade.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from ..shared.utils import constraints
+from ..shared.utils import assertions
 from ..shared import bases
 
 
@@ -55,6 +56,12 @@ class Upgrade(bases.MultiHeaderBase):
     """
 
     name = 'upgrade'
+
+    def check_value(self, value):
+        assertions.must_be_tuple_of(value, 2)
+        protocol, version = value
+        assertions.must_be_token(protocol)
+        version is None or assertions.must_be_token(version)
 
     def value_str(self, value):
         protocol, version = value
