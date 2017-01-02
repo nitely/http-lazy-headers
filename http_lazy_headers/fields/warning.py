adc6415b14c302ab91ed102760d220074f612924
nitely <ecastroborsani@gmail.com>
2017-01-02 01:26:55 -0300

    warning validate host

--- http_lazy_headers/fields/warning.py
+++ http_lazy_headers/fields/warning.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 
+from ..shared.common import hosts
 from ..shared.common import dates
 from ..shared.generic import cleaners
 from ..shared.utils import checkers
@@ -10,8 +11,20 @@ from .. import exceptions
 from ..shared import bases
 
 
-def warning(code, host_or_token, message, date_time=None, port=None):
-    return code, (host_or_token, port), message, date_time
+def warning(
+        code,
+        message,
+        host=None,
+        pseudonym=None,
+        date_time=None):
+    assert pseudonym or host
+    assert not (pseudonym and host)
+
+    return (
+        code,
+        (host or hosts.host(), pseudonym),
+        message,
+        date_time)
 
 
 class Warning(bases.MultiHeaderBase):
@@ -32,7 +45,7 @@ class Warning(bases.MultiHeaderBase):
         Warning([
             warning(
                 code=112,
-                host_or_token='-',
+                pseudonym='-',
                 message='network down',
                 date_time=datetime.now())
         ])
@@ -40,7 +53,7 @@ class Warning(bases.MultiHeaderBase):
         Warning([
             warning(
                 code=112,
-                host_or_token='-',
+                pseudonym='-',
                 message='network down')
         ])
 
@@ -68,7 +81,7 @@ class Warning(bases.MultiHeaderBase):
         assertions.must_be_tuple_of(value, 4)
         assertions.must_be_tuple_of(value[1], 2)
 
-        code, (host_or_token, port), message, date_time = value
+        code, (host, pseudonym), message, date_time = value
 
         assertions.must_be_int(code)
         assertions.assertion(
@@ -76,35 +89,37 @@ class Warning(bases.MultiHeaderBase):
             '"{}" received, 3 chars int '
             'was expected'.format(code))
         assertions.assertion(
-            checkers.is_token(host_or_token) or
-            checkers.is_uri(host_or_token),
-            '"{}" received, token or host '
-            'was expected'.format(host_or_token))
-        not port or assertions.must_be_int(port)
+            (pseudonym and
+             host == hosts.host()) or
+            (not pseudonym and
+             host != hosts.host()),
+            '"{}" and "{}" received, either '
+            'pseudonym or host was expected'
+            .format(pseudonym, host))
+        pseudonym is None or assertions.must_be_token(pseudonym)
+        hosts.check_host(host)
         assertions.must_be_ascii(message)
         (date_time is None or
          assertions.must_be_datetime(date_time))
 
     def value_str(self, value):
-        code, (host_or_token, port), message, date_time = value
+        code, (host, pseudonym), message, date_time = value
 
-        if port is not None:
-            host_or_token = '{}:{}'.format(
-                host_or_token, port)
+        agent = pseudonym or hosts.format_host(host)
 
         if date_time:
             return (
-                '{code} {token} '
+                '{code} {agent} '
                 '{message} "{date_time}"'
                 .format(
                     code=code,
-                    token=host_or_token,
+                    agent=agent,
                     message=parsers.quote(message),
                     date_time=dates.format_date(date_time)))
         else:
-            return '{code} {token} {message}'.format(
+            return '{code} {agent} {message}'.format(
                 code=code,
-                token=host_or_token,
+                agent=agent,
                 message=parsers.quote(message))
 
     def values_str(self, values):
@@ -130,15 +145,15 @@ class Warning(bases.MultiHeaderBase):
                 date = None
 
         try:
-            agent, port = agent.split(':', 1)
-        except ValueError:
-            port = None
+            host = hosts.clean_host(agent)
+        except exceptions.HeaderError:
+            constraints.must_be_token(agent)
+            pseudonym = agent
+            host = hosts.host()
+        else:
+            pseudonym = None
 
         constraints.must_be_in(code, self.codes_str)
-        constraints.constraint(
-            checkers.is_uri(agent) or
-            checkers.is_token(agent),
-            'Agent value is not a valid uri or token')
         constraints.constraint(
             checkers.is_quoted_string(text),
             'Text value must be a quoted-string')
@@ -149,14 +164,10 @@ class Warning(bases.MultiHeaderBase):
              date.endswith('"')),
             'Date must be a quoted date')
 
-        # May be empty
-        if port:
-            port = cleaners.clean_number(port, max_chars=5)
-
         code = cleaners.clean_number(code, max_chars=3)
         text = parsers.dequote(text)
 
         if date is not None:
             date = dates.clean_date_time(date[1:-1])
 
-        return code, (agent, port), text, date
+        return code, (host, pseudonym), text, date
--- tests/tests_fields.py
+++ tests/tests_fields.py
@@ -898,16 +898,34 @@ class WarningTest(FieldTestCase):
                 '112 - "network down" "Sat, 25 Aug 2012 23:34:45 GMT", 112 - "err"',
                 '112 - "foo"'],
             (
-                (112, ('-', None), 'network down', self.date),
-                (112, ('-', None), 'err', None),
-                (112, ('-', None), 'foo', None)))
+                (112,
+                 ((None, None, None, None, None, None), '-'),
+                 'network down',
+                 self.date),
+                (112,
+                 ((None, None, None, None, None, None), '-'),
+                 'err',
+                 None),
+                (112,
+                 ((None, None, None, None, None, None), '-'),
+                 'foo',
+                 None)))
 
     def test_str(self):
         self.assertFieldStrEqual(
             (
-                (112, ('-', None), 'network down', self.date),
-                (112, ('-', None), 'err', None),
-                (112, ('-', None), 'foo', None)),
+                (112,
+                 ((None, None, None, None, None, None), '-'),
+                 'network down',
+                 self.date),
+                (112,
+                 ((None, None, None, None, None, None), '-'),
+                 'err',
+                 None),
+                (112,
+                 ((None, None, None, None, None, None), '-'),
+                 'foo',
+                 None)),
             'warning: 112 - "network down" "Sat, 25 Aug 2012 23:34:45 GMT", '
             '112 - "err", 112 - "foo"')
 
