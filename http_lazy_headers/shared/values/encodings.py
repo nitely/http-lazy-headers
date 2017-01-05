8933e553607dd001ae538c5004d99e695daaa030
nitely <ecastroborsani@gmail.com>
2017-01-05 00:14:04 -0300

    shared.values transfer and content encodings

--- http_lazy_headers/shared/values/encodings.py
+++ http_lazy_headers/shared/values/encodings.py
@@ -1,15 +1,36 @@
 # -*- coding: utf-8 -*-
 
 
-class Encodings:
+class CEncodings:
 
-    # http://www.iana.org/assignments/http-parameters/http-parameters.xhtml
+    br = 'br'
+    compress = 'compress'
+    deflate = 'deflate'
+    exi = 'exi'
+    gzip = 'gzip'
+    identity = 'identity'
+    pack200_gzip = 'pack200-gzip'
+    x_compress = 'x-compress'
+    x_gzip = 'x-gzip'
+
+
+class TEncodings:
 
     chunked = 'chunked'
-    gzip = 'gzip'
-    deflate = 'deflate'
     compress = 'compress'
-    identity = 'identity'  # Accept-Encoding only
+    deflate = 'deflate'
+    gzip = 'gzip'
+    identity = 'identity'
+    x_compress = 'x-compress'
+    x_gzip = 'x-gzip'
+
+
+class Encodings(CEncodings, TEncodings):
+    """
+    All encodings, including content and transfer
+
+    `Ref. <http://www.iana.org/assignments/http-parameters/http-parameters.xhtml>`_
+    """
 
 
 ENCODING_VALUES = {
