68b60a279f53f1c8f997ee8f932040026855c6ad
nitely <ecastroborsani@gmail.com>
2017-01-15 04:10:57 -0300

    tests last_modified

---
+++ tests_fields_/tests_last_modified.py
@@ -0,0 +1,94 @@
+# -*- coding: utf-8 -*-
+
+import datetime
+
+import http_lazy_headers as hlh
+
+from . import utils
+
+
+class LastModifiedTest(utils.FieldTestCase):
+
+    field = hlh.LastModified
+
+    def test_raw_values(self):
+        self.assertFieldRawEqual(
+            ['Tue, 15 Nov 1994 08:12:31 GMT'],
+            (datetime.datetime(
+                year=1994,
+                month=11,
+                day=15,
+                hour=8,
+                minute=12,
+                second=31),))
+
+        self.assertFieldRawEqual(
+            ['Sunday, 06-Nov-94 08:49:37 GMT'],
+            (datetime.datetime(
+                year=1994,
+                month=11,
+                day=6,
+                hour=8,
+                minute=49,
+                second=37),))
+
+        self.assertFieldRawEqual(
+            ['Sun Nov  6 08:49:37 1994'],
+            (datetime.datetime(
+                year=1994,
+                month=11,
+                day=6,
+                hour=8,
+                minute=49,
+                second=37),))
+
+    def test_str(self):
+        self.assertFieldStrEqual(
+            (datetime.datetime(
+                year=1994,
+                month=11,
+                day=15,
+                hour=8,
+                minute=12,
+                second=31),),
+            'last-modified: Tue, 15 Nov 1994 08:12:31 GMT')
+
+    def test_raw_empty(self):
+        """
+        Should NOT allow empty raw value
+        """
+        self.assertRaisesHeaderError([''])
+
+    def test_empty(self):
+        """
+        Should NOT allow empty value
+        """
+        self.assertRaisesInternalError(())
+
+    def test_raw_bad_values(self):
+        """
+        Should not allow bad raw values
+        """
+        self.assertRawOK(['Tue, 15 Nov 1994 08:12:31 GMT'])
+        self.assertRaisesHeaderError(['Tue, 15 Nov'])
+        self.assertRaisesHeaderError(['Sun, 15 Nov 1994 08:12:31 GMT'])
+        self.assertRaisesHeaderError(['Tue, 15 Nov 1994 08:12:31 UTC'])
+        self.assertRaisesHeaderError(['Tue, 15 Nov 1 08:12:31 UTC'])
+
+    def test_bad_values(self):
+        """
+        Should not allow bad values
+        """
+        good_date = datetime.datetime(
+            year=1994,
+            month=11,
+            day=15,
+            hour=8,
+            minute=12,
+            second=31)
+        self.assertOK([good_date])
+        self.assertRaisesInternalError([1])
+        self.assertRaisesInternalError(['foo'])
+        self.assertRaisesInternalError([None])
+        self.assertRaisesInternalError([good_date, good_date])
+
